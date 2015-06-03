c220ea97a4e55794ba0dabd6a14ade3589f8fbae
jleaniz <juan.leaniz@gmail.com>
2015-06-03 15:08:59 -0400

    New parser
    New parser using regexp and returning pyspark SQL Rows

--- SImageInfo.py
+++ SImageInfo.py
@@ -1,25 +1,23 @@
 import utils
 from pyspark import SparkContext
+from pyspark.sql import SQLContext
+from pyspark.sql.types import *
 
 if __name__ == '__main__':
-    '''
-    If you can package your module into a .egg or .zip file, you should be able to list it in pyFiles when constructing your SparkContext (or you can add it later through
-    sc.addPyFile).
-    For Python libraries that use setuptools, you can run python setup.py bdist_egg to build an egg distribution.
-    Another option is to install the library cluster-wide, either by using pip/easy_install on each machine or by sharing a Python installation over a cluster-wide
-    filesystem (like NFS).
-    '''
+
     sc = SparkContext("local", "SparkVolatility", pyFiles=['utils.py'])
+#    sqlContext = SQLContext(sc)
     images = sc.textFile('/user/cloudera/imgnames.txt')
 
     modules = ['imageinfo']
     volatility = utils.SparkVolatility(modules)
 
-    if 'imageinfo' in volatility.modules:
-        rdd = images.map(volatility.ImageInfo)
-        volatilityOutput = rdd.collect()
-        for i in volatility.parseImageInfo(volatilityOutput):
-            print i
+    rdd = images.map(volatility.Execute)
+    #volatilityOutput = rdd.collect() #volatilityOutput is a list of lists with the imageInfo output
 
-    #rdd = images.map(utils.ImageInfo)
+    if volatility.checkPathHadoop('hdfs:///user/cloudera/mytest'):
+        volatility.rmHadoop("hdfs:///user/cloudera/mytest")
+
+    rdd.saveAsTextFile("hdfs:///user/cloudera/mytest")
+
     sc.stop()
--- utils.py
+++ utils.py
@@ -5,11 +5,29 @@ import volatility.registry as registry
 import volatility.commands as commands
 import volatility.addrspace as addrspace
 import mrjob.fs.hadoop
+import re
+from pyspark.sql import Row
 
 class SparkVolatility:
 
     def __init__(self, volatilityModules):
         self.modules = volatilityModules
+
+    def checkPathHadoop(self, hdfs_path):
+        fs = mrjob.fs.hadoop.HadoopFilesystem(['hadoop'])
+        try:
+            fs.path_exists(hdfs_path)
+            return True
+        except:
+            return False
+
+    def rmHadoop(self, hdfs_path):
+        fs = mrjob.fs.hadoop.HadoopFilesystem(['hadoop'])
+        try:
+            fs.rm(hdfs_path)
+            return True
+        except:
+            return False
 
     def CopyHadoopLocal(self, hdfs_path, dest_path, img_name):
         # Read the image file from HDFS and write to /dev/shm
@@ -23,7 +41,31 @@ class SparkVolatility:
         except:
             return False
 
-    def RunVolatility(self, module, img_path):
+    def parseImageInfo(self, lines):
+        PROFILES_PATTERN = "(\w+)(\s)(\w+)(\S+)(\s)(\:)(\s)(\S+)"
+        KDBG_PATTERN = "(\KDBG)(\s:\s)(\w+)"
+
+        for line in lines:
+            profile = re.search(PROFILES_PATTERN, line)
+            kdbg = re.search(KDBG_PATTERN, line)
+
+            if profile:
+                if 'Profile' in line:
+                    m_profile = profile.group(8)
+            
+            if kdbg:
+                m_kdbg = kdbg.group(3)
+
+        return Row(
+            profile = m_profile,
+            kdbg = m_kdbg
+            )
+        
+
+    def RunVolatility(self, modules, img_path):
+        data = ""
+        lines_strip = []
+
          # Volatility blackbox stuff
         registry.PluginImporter()
         config = conf.ConfObject()
@@ -35,36 +77,35 @@ class SparkVolatility:
 
         # Build volatility command and execute
         cmds = registry.get_plugin_classes(commands.Command, lower = True)
-        command = cmds[module](config)
-        data = command.calculate()
+        for module in modules:
+            command = cmds[module](config)
+            data = command.calculate()
 
-        # Store output in a temporary file and read it
-        f = tempfile.NamedTemporaryFile(mode='w+',delete=True)
-        command.render_text(f, data)
-        f.seek(0)
-        lines_strip = []
-        lines = f.readlines()
-        for i in lines:
-            lines_strip.append(img_path+':'+i.strip())
+            # Store output in a temporary file and read it
+            f = tempfile.NamedTemporaryFile(mode='w+',delete=True)
+            command.render_text(f, data)
+            f.seek(0)
+            lines = f.readlines()
+            f.close()
 
-        f.close()
-        return lines_strip
+            # append the image name before the results
+            lines_strip.append(img_path)
 
-    def ImageInfo(self, img_path):
+            # build a list with each value prefixed by the img path
+            for i in lines:
+                lines_strip.append(i.strip())
+
+            if module == 'imageinfo':
+                row = self.parseImageInfo(lines_strip)
+
+        return row
+
+    def Execute(self, img_path):
         if self.CopyHadoopLocal('hdfs:///user/cloudera/', '/dev/shm/', img_path):
-            output = self.RunVolatility('imageinfo', 'file:///dev/shm/' + img_path)
+            output = self.RunVolatility(self.modules, 'file:///dev/shm/' + img_path)
             os.remove('/dev/shm/' + img_path)
             return output #if this retunrs a tuple (key,value) we can use transformations in Pair RDDs
 
-    def parseImageInfo(self, ImageInfoOutput):
-        parsedOutput = []
-        for lines in ImageInfoOutput:
-            for line in lines:
-                if len(line.split(':')) == 4:
-                    (prefix, img_path, key, value) = line.split(':')
-                    if 'KDBG' in key:
-    #                    print "%s:%s:%s" % (img_path, key, value)
-                        # this will need rework if ImageInfo returns a tuple (key, vlue)
-                        kdbg = "%s:%s:%s" % (img_path, key, value)
-                        parsedOutput.append(kdbg)
-        return parsedOutput
+
+
+
