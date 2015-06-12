3888fb444b846b4955d983e1291f4c67f713d921
jleaniz <juan.leaniz@gmail.com>
2015-06-12 17:08:57 -0400

    test file

--- test.py
+++ test.py
@@ -1,10 +1,15 @@
+import utils
 from pyspark import SparkContext
 from pyspark.sql import SQLContext
 from pyspark.sql.types import *
 
 if __name__ == '__main__':
-    sc = SparkContext("local", "SparkVolatility", pyFiles=['utils.py','hdfs.py','parser.py'])
+    volatility = utils.SparkVolatility('pslist')
     sqlContext = SQLContext(sc)
-    df = sqlContext.load('Volatility/imageinfo')
-    df.show()
-    sc.stop()
+
+    df = sqlContext.load('Volatility/pslist')
+    sqlContext.registerDataFrameAsTable(df, "pslist")
+    pslist = sqlContext.sql("SELECT proclist FROM pslist WHERE image LIKE '" + "ds_fuzz_hidden_proc.img" + "'") 
+
+    print pslist.collect()[0][0]
+    
