4a0d8f2910c09d84f74aae9ae1a588bfea5a98b8
Juan Leaniz <juan.leaniz@gmail.com>
2015-07-15 11:32:27 -0400

    reformat pep8

--- driver.py
+++ driver.py
@@ -10,57 +10,37 @@ from pyspark import SparkConf
 from pyspark.sql import SQLContext
 from pyspark.sql.types import *
 
+
 def main():
-	conf = (SparkConf()
-		.setMaster("spark://mtl-srm-cdh01.ubisoft.org:7077")
-		.setAppName("SRM-Analytics")
-		.set("spark.driver.cores", "1")
-		.set("spark.driver.maxResultSize", "200m")
-		.set("spark.driver.memory", "512m")
-		###########################################################
-		#### If spark worker/executor java heap size is set in CHD
-		###  and is lower these won't take effect
-		.set("spark.worker.memory", "512m") 
-		.set("spark.executor.memory", "512m")
-		###########################################################
-		.set("spark.executor.cores", "4")
-		.set("spark.cores.max", "16")
-		.set("spark.akka.timeout", "3000")
-		.set("spark.network.timeout", "3000")
-		.set("spark.core.connection.ack.wait.timeout", "3000")
-		.set("spark.storage.memoryFraction", "0.5")
-		.set("spark.default.parallelism", "48"))
+  cliparser = argparse.ArgumentParser(description='SRM Security Analytics')
+  cliparser.add_argument('-i', '--ingest', action='append',
+                         choices=['c2', 'openphish', 'alienvault_otx', 'bluecoat', 'iptables', 'imageinfo', 'pslist'],
+                         required=True, help='Ingest raw logs into HDFS (saves Parquet files)')
+  cliparser.add_argument('-s', '--host', action='append',
+                         required=True, help='hostname to ingest logs from, has to match dir in /home/cloudera/fw/raw')
 
-	sc = SparkContext(conf = conf)
+  args = cliparser.parse_args()
 
-	cliparser = argparse.ArgumentParser(description='SRM Security Analytics')
-	cliparser.add_argument('-i', '--ingest', action='append', 
-		choices=['c2', 'openphish', 'alienvault_otx', 'bluecoat', 'iptables', 'imageinfo', 'pslist'],
-		required=True, help='Ingest raw logs into HDFS (saves Parquet files)')
-	cliparser.add_argument('-s', '--host', action='append',
-		required=True, help='hostname to ingest logs from, has to match dir in /home/cloudera/fw/raw')
+  for arg in args.ingest:
+    if arg == 'iptables':
+      for host in args.host:
+        print 'Ingesting iptables logs for ', (host)
+        iptables.save_log(sc, host)
+    elif arg == 'bluecoat':
+      print 'Ingesting Blue Coat ProxySG access logs...'
+      proxysg.save_access_log(sc)
+    elif arg == 'alienvault_otx':
+      print 'Updating local AlienVault OTX db...'
+      aotx.update_alienvault_otx(sc)
+    elif arg == 'openphish':
+      print 'Updating local OpenPhish db...'
+      openphish.update_openphish(sc)
+    elif arg == 'c2':
+      print 'Updating local c2 db...'
+      c2.update_c2_feeds(sc)
 
-	args = cliparser.parse_args()
+  sc.stop()
 
-	for arg in args.ingest:
-		if arg == 'iptables':
-			for host in args.host:
-				print 'Ingesting iptables logs for ', (host)
-				iptables.save_log(sc, host)
-		elif arg == 'bluecoat':
-			print 'Ingesting Blue Coat ProxySG access logs...'
-			proxysg.save_access_log(sc)
-		elif arg == 'alienvault_otx':
-			print 'Updating local AlienVault OTX db...'
-			aotx.update_alienvault_otx(sc)
-		elif arg == 'openphish':
-			print 'Updating local OpenPhish db...'
-			openphish.update_openphish(sc)
-		elif arg == 'c2':
-			print 'Updating local c2 db...'
-			c2.update_c2_feeds(sc)
 
-	sc.stop()
-	
 if __name__ == '__main__':
-	main()
+  main()
--- srm/ingest/bluecoat/proxysg.py
+++ srm/ingest/bluecoat/proxysg.py
@@ -4,25 +4,21 @@ from pyspark import SparkContext
 from pyspark.sql import SQLContext
 from pyspark.sql.types import *
 
-
 def save_access_log(sContext):
     # alter table sg_accesslog add partition (p_year='2015', p_month='07', p_day='02');
     # load data inpath '/user/cloudera/proxy/sg_accesslog_p/p_year=2015/p_month=07/p_day=02' into table sg_accesslog
     # partition (p_year='2015', p_month='07', p_day='02');
     sqlCtx = SQLContext(sContext)
     sqlCtx.setConf('spark.sql.parquet.compression.codec', 'snappy')
-
-
-path = '/mnt/hdfs/user/cloudera/proxy/raw/'
-months = os.listdir(path + host + '/2015/')
-
-for month in months:
-    days = os.listdir(path + host + '/2015/' + month)
-    for day in days:
-        if os.listdir(path + host + '/2015/' + month + '/' + day):
-            access_log_rdd = sContext.textFile('/user/cloudera/proxy/raw/' + host + '/2015/'
-                                               + month + '/' + day + '/*').repartition(sContext.defaultParallelism)
-            parsed_rdd = access_log_rdd.mapPartitions(parser.parseIPTables)
-            df = parsed_rdd.toDF()
-            df.save('/user/cloudera/proxy/accesslog/p_year=2015/p_month=' + str(int(month))
-                    + '/p_day=' + str(int(day)), 'parquet', 'append')
+    path = '/mnt/hdfs/user/cloudera/proxy/raw/'
+    months = os.listdir(path + host + '/2015/')
+    for month in months:
+        days = os.listdir(path + host + '/2015/' + month)
+        for day in days:
+            if os.listdir(path + host + '/2015/' + month + '/' + day):
+                access_log_rdd = sContext.textFile('/user/cloudera/proxy/raw/' + host + '/2015/'
+                                                   + month + '/' + day + '/*').repartition(sContext.defaultParallelism)
+                parsed_rdd = access_log_rdd.mapPartitions(parser.parseIPTables)
+                df = parsed_rdd.toDF()
+                df.save('/user/cloudera/proxy/accesslog/p_year=2015/p_month=' + str(int(month))
+                        + '/p_day=' + str(int(day)), 'parquet', 'append')
