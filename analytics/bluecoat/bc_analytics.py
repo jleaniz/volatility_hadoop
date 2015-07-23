798122e3df343b7e8155bbc79649dc2f8484a979
jleaniz <juan.leaniz@gmail.com>
2015-07-23 16:39:56 -0400

    Bluecoat analytics

--- analytics/bluecoat/bc_analytics.py
+++ analytics/bluecoat/bc_analytics.py
@@ -14,18 +14,36 @@
 # You should have received a copy of the GNU General Public License
 # along with BDSA.  If not, see <http://www.gnu.org/licenses/>.
 #
-from pyspark import SparkContext
 from pyspark.sql import SQLContext
 from pyspark.sql.types import *
 
-if __name__ == '__main__':
-    SparkContext.setSystemProperty('spark.executor.memory', '2g')
-    sc = SparkContext("local[8]", "SparkVolatility", pyFiles=['parser.py'])
-    sqlContext = SQLContext(sc)
 
-    df = sqlContext.load('impala_tables/bluecoat')
-    sqlContext.registerDataFrameAsTable(df, "accesslog")
-    data = sqlContext.sql(
-        "SELECT host, count(*) as hits FROM accesslog WHERE action LIKE '%DENIED%' GROUP BY host ORDER BY hits DESC")
-    data.show()
-    sc.stop()
+def getClientsByTransferP(sc, parquetFiles, number, month):
+    '''
+    Get a list of client ips ordered by bytes transfered
+    :param sc: SparkContext
+    :param number: limit results to 'number'
+    :return: DataFrame
+    '''
+
+    # Creat Spark SQL context
+    sqlctx = SQLContext(sc)
+    # Load files into a DataFrame
+    df = sqlctx.parquetFile(parquetFiles)
+    # Register temporary in-memory table
+    sqlctx.registerDataFrameAsTable(df, 'sgos')
+
+    # Query using Spark SQL
+    data = sqlctx.sql(
+        'select p_day, clientip, host, cast(csbytes as bigint) as bytes \
+        from sgos_accesslog \
+        where urischeme="ssl" and p_month=%s or method="POST" and p_month=%s \
+        group by p_day, clientip, host, cast(csbytes as bigint) \
+        order by cast(csbytes as bigint) desc \
+        limit %s' % (month, month, number)
+    )
+
+    # Save to JSON file in HDFS
+    df.save('test.json', 'json', 'overwrite')
+
+    return data
