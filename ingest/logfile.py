9f81bf21fda928aef1c110fff6bf6ac95ef0f73a
jleaniz <juan.leaniz@gmail.com>
2015-07-29 13:23:37 -0400

    added accumulator counter for parser

--- ingest/logfile.py
+++ ingest/logfile.py
@@ -21,10 +21,6 @@ import threading
 import Queue
 
 class LogFile(object):
-    '''
-    This class handles log files
-    '''
-
     def __init__(self, path, parser, sc, destPath):
         self.localHdfs = '/mnt/hdfs'
         self.path = None
@@ -33,54 +29,49 @@ class LogFile(object):
         self.sContext = sc
         self.destPath = destPath
 
-    def parallelsave(self, localPath, year, month, day, r_queue):
-        if os.listdir('%s/%s/%s/%s' % (localPath, year, month, day)):
-            rdd = self.sContext.textFile('%s/%s/%s/%s' % (self.path, year, month, day))
-            if self.type is 'proxysg':
-                parsed_rdd = rdd.mapPartitions(self.parser.parseBCAccessLog)
-                df = parsed_rdd.toDF()
-                df.save('%s/proxysg/year=%s/month=%s/day=%s' % (self.destPath, year, month, day), 'parquet',
-                        'append')
-
-            if self.type is 'iptables':
-                parsed_rdd = rdd.mapPartitions(self.parser.parseIPTables)
-                df = parsed_rdd.toDF()
-                if 'onl' in self.path:
-                    df.save('%s/onl/year=%s/month=%s/day=%s' % (self.destPath, year, month, day), 'parquet',
-                            'append')
-                if 'onbe' in self.path:
-                    df.save('%s/onbe/year=%s/month=%s/day=%s' % (self.destPath, year, month, day), 'parquet',
-                            'append')
-                if 'off' in self.path:
-                    df.save('%s/off/year=%s/month=%s/day=%s' % (self.destPath, year, month, day), 'parquet',
+    def parallelsave(self, localPath, year, month, days, r_queue):
+        for day in days:
+            days.remove(day)
+            if os.listdir('%s/%s/%s/%s' % (localPath, year, month, day)):
+                rdd = self.sContext.textFile('%s/%s/%s/%s' % (self.path, year, month, day))
+                if self.type is 'proxysg':
+                    parsed_rdd = rdd.mapPartitions(self.parser.parseBCAccessLog)
+                    df = parsed_rdd.toDF()
+                    df.save('%s/proxysg/year=%s/month=%s/day=%s' % (self.destPath, year, month, day), 'parquet',
                             'append')
 
-            if self.type is 'apacheAccessLog':
-                parsed_rdd = rdd.mapPartitions(self.parser.parseApacheAL())
-                df = parsed_rdd.toDF()
-                df.save('%s/apache/year=%s/month=%s/day=%s' % (self.destPath, year, month, day), 'parquet',
-                        'append')
+                if self.type is 'iptables':
+                    parsed_rdd = rdd.mapPartitions(self.parser.parseIPTables)
+                    df = parsed_rdd.toDF()
+                    if 'onl' in self.path:
+                        df.save('%s/onl/year=%s/month=%s/day=%s' % (self.destPath, year, month, day), 'parquet',
+                                'append')
+                    if 'onbe' in self.path:
+                        df.save('%s/onbe/year=%s/month=%s/day=%s' % (self.destPath, year, month, day), 'parquet',
+                                'append')
+                    if 'off' in self.path:
+                        df.save('%s/off/year=%s/month=%s/day=%s' % (self.destPath, year, month, day), 'parquet',
+                                'append')
 
-            if self.type is 'bashlog':
-                parsed_rdd = rdd.mapPartitions(self.parser.parseBash)
-                df = parsed_rdd.toDF()
-                df.save('%s/bashlog/year=%s/month=%s/day=%s' % (self.destPath, year, month, day), 'parquet',
-                        'append')
+                if self.type is 'apacheAccessLog':
+                    parsed_rdd = rdd.mapPartitions(self.parser.parseApacheAL())
+                    df = parsed_rdd.toDF()
+                    df.save('%s/apache/year=%s/month=%s/day=%s' % (self.destPath, year, month, day), 'parquet',
+                            'append')
 
+                if self.type is 'bashlog':
+                    parsed_rdd = rdd.mapPartitions(self.parser.parseBash)
+                    df = parsed_rdd.toDF()
+                    df.save('%s/bashlog/year=%s/month=%s/day=%s' % (self.destPath, year, month, day), 'parquet',
+                            'append')
+
+        print 'Completed tasks for date: %s-%s-%s' % (year, month, day)
+        print 'Success: %s' % (self.parser.success.value)
+        self.parser.success = self.sContext.accumulator(0)
         r_queue.put((day, 'done'))
 
     def saveLogByDate(self):
-        '''
-        This function will parse logs and save them
-        to HDFS in parquet format partitioned by date
 
-        The path containing the log files must be structured as follows:
-        /path/to/logs/year/month/day/logfile.(gz,txt,etc)
-
-        :param sContext: A Spark context
-        :param path: The HDFS path containing the log files
-        :return: None
-        '''
         sqlCtx = SQLContext(self.sContext)
         sqlCtx.setConf('spark.sql.parquet.compression.codec', 'snappy')
         localPath = self.localHdfs + self.path
@@ -89,14 +80,9 @@ class LogFile(object):
             months = os.listdir('%s/%s' % (localPath, year))
             for month in months:
                 days = os.listdir('%s/%s/%s' % (localPath, year, month))
-                for day in days:
-                    q = Queue.Queue()
-                    threads = [threading.Thread(target=self.parallelsave, args=(localPath,year,month,day,q)) for i in range(4)]
-                    for thread in threads:
-                        thread.start()
+                q = Queue.Queue()
+                threads = [threading.Thread(target=self.parallelsave, args=(localPath,year,month,days,q)) for i in range(8)]
+                for thread in threads:
+                    thread.start()
+                r = q.get()
 
-                    r = q.get()
-
-                    print 'Completed tasks for date: %s-%s-%s' % (year, month, day)
-                    print 'Success: %s' % (self.parser.success.value)
-                    self.parser.success = self.sContext.accumulator(0)
