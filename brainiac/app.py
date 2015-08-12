73d3acbb937c7f4fbc4dffcc1c85263ade89910c
jleaniz <juan.leaniz@gmail.com>
2015-08-11 21:27:05 -0400

    fix path

--- brainiac/app.py
+++ brainiac/app.py
@@ -17,7 +17,7 @@ def generateJSON():
     rdd = analytics_engine.getVPNLoginsByUser('juan.leaniz@ubisoft.com')
     def generate():
         for doc in rdd.collect():
-            yield json.dumps(doc)
+            yield doc + '\n'
     return Response(generate(), mimetype='application/json')
 
 '''
