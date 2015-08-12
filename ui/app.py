436ab745fed1cbfc831c402b74feb35c70927f86
jleaniz <juan.leaniz@gmail.com>
2015-08-12 12:24:42 -0400

    flask bootstrap + flask_nav

--- ui/app.py
+++ ui/app.py
@@ -76,8 +76,8 @@ def vpn_display():
         #return redirect(url_for('main.vpnJSON', username=form.name.data
         #                        )
         #                )
-
-        return render_template("vpn.html", form=form)
+        render_template("vpn.html", form=form)
+    return render_template("vpn.html", form=form)
 
 # Our index-page just shows a quick explanation. Check out the template
 # "templates/index.html" documentation for more details.
