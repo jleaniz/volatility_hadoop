5c52bd28f1b0008e2d45b7e27dfdcb539b5b7bfd
jleaniz <juan.leaniz@gmail.com>
2015-08-12 10:10:04 -0400

    flask bootstrap + flask_nav

--- ui/app.py
+++ ui/app.py
@@ -1,7 +1,37 @@
 from flask import Flask, request, render_template
 from flask import Response
 from flask import Blueprint
+from flask_bootstrap import __version__ as FLASK_BOOTSTRAP_VERSION
+from flask_nav.elements import Navbar, View, Subgroup, Link, Text, Separator
+from flask_bootstrap import Bootstrap
+from flask_nav import Nav
+
 main = Blueprint('main', __name__)
+nav = Nav()
+
+# We're adding a navbar as well through flask-navbar. In our example, the
+# navbar has an usual amount of Link-Elements, more commonly you will have a
+# lot more View instances.
+nav.register_element('frontend_top', Navbar(
+    View('Flask-Bootstrap', '.index'),
+    View('Home', '.index'),
+    View('Forms Example', '.example_form'),
+    View('Debug-Info', 'debug.debug_root'),
+    Subgroup(
+        'Docs',
+        Link('Flask-Bootstrap', 'http://pythonhosted.org/Flask-Bootstrap'),
+        Link('Flask-AppConfig', 'https://github.com/mbr/flask-appconfig'),
+        Link('Flask-Debug', 'https://github.com/mbr/flask-debug'),
+        Separator(),
+        Text('Bootstrap'),
+        Link('Getting started', 'http://getbootstrap.com/getting-started/'),
+        Link('CSS', 'http://getbootstrap.com/css/'),
+        Link('Components', 'http://getbootstrap.com/components/'),
+        Link('Javascript', 'http://getbootstrap.com/javascript/'),
+        Link('Customize', 'http://getbootstrap.com/customize/'),
+    ),
+    Text('Using Flask-Bootstrap {}'.format(FLASK_BOOTSTRAP_VERSION)),
+))
 
 import logging
 logging.basicConfig(level=logging.INFO)
@@ -28,11 +58,23 @@ def generateJSONArray(username):
 def vpnDisplay():
     return render_template("vpn.html")
 
+# Our index-page just shows a quick explanation. Check out the template
+# "templates/index.html" documentation for more details.
+@frontend.route('/')
+def index():
+    return render_template('index.html')
+
 def create_app(spark_context, dataset_path):
     global analytics_engine
 
     analytics_engine = AnalyticsEngine(spark_context, dataset_path)
 
     app = Flask(__name__)
+
+    Bootstrap(app)
+
     app.register_blueprint(main)
+
+    nav.init_app(app)
+
     return app
