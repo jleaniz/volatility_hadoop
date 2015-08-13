07ab0574afd54d99909e5ebd07894b612ddcbf4d
jleaniz <juan.leaniz@gmail.com>
2015-08-12 21:13:07 -0400

    add proxy malware lookup by user

--- ui/engine.py
+++ ui/engine.py
@@ -91,9 +91,9 @@ class AnalyticsEngine:
         # Register DataFrame as a Spark SQL Table
         self.sqlctx.registerDataFrameAsTable(self.proxyDF, 'proxy')
 
-        query = ( "select clientip, username, host, port, path, query, count(*) as hits from proxy"
+        query = ( "select username, host, port, path, query, count(*) as hits from proxy"
             " where username='%s' and category like '%s'"
-            " group by clientip, username, host, port, path, query"
+            " group by username, host, port, path, query"
             #" order by hits desc"
             " limit 50" % (username, '%Mal%') )
         logger.info(query)
