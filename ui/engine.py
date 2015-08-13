e6289724dd48a0ed692e64679824090bb44070a0
jleaniz <juan.leaniz@gmail.com>
2015-08-12 21:11:14 -0400

    add proxy malware lookup by user

--- ui/engine.py
+++ ui/engine.py
@@ -94,7 +94,7 @@ class AnalyticsEngine:
         query = ( "select clientip, username, host, port, path, query, count(*) as hits from proxy"
             " where username='%s' and category like '%s'"
             " group by clientip, username, host, port, path, query"
-            " order by hits desc"
+            #" order by hits desc"
             " limit 50" % (username, '%Mal%') )
         logger.info(query)
 
