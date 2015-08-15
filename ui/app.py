2d5fc5cf7f2835ea7b7d678d038562325d007800
jleaniz <juan.leaniz@gmail.com>
2015-08-15 00:36:43 -0400

    test file download

--- ui/app.py
+++ ui/app.py
@@ -89,11 +89,11 @@ from engine import AnalyticsEngine
 
 @main.app_errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html', e), 404
+    return render_template('404.html'), 404
 
 @main.app_errorhandler(500)
 def internal_server_error(e):
-    return render_template('500.html', e), 500
+    return render_template('500.html'), 500
 
 @main.route("/api/vpn/byUser/<username>")
 def vpnJSON(username):
