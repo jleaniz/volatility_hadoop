e218be6044790b4716d55d9b28905526b7e6096d
jleaniz <juan.leaniz@gmail.com>
2015-08-25 11:37:31 -0400

    index fix

--- ui/app.py
+++ ui/app.py
@@ -58,9 +58,9 @@ nav = Nav()
 # navbar has an usual amount of Link-Elements, more commonly you will have a
 # lot more View instances.
 nav.register_element('frontend_top', Navbar(
-    View('BDSA', '/'),
-    View('Home', '/'),
-    View('Dashboard', '/'),
+    View('BDSA', '.index'),
+    View('Home', '.index'),
+    View('Dashboard', '.index'),
     Subgroup(
         'Search',
         Link('Custom query', '/search'),
