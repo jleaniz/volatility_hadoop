2b67baf7e926dc394b5a5412bc4a57db1dad3f9f
jleaniz <juan.leaniz@gmail.com>
2015-08-26 19:57:49 -0400

    caching

--- ui/engine.py
+++ ui/engine.py
@@ -267,7 +267,7 @@ class AnalyticsEngine:
                 filteredDF = tempDF.filter('year=%s and month=%s and day=%s' %( day.year, str(day).split('-')[1], str(day).split('-')[2] ) )
                 self.sqlctx.registerDataFrameAsTable(filteredDF, table)
                 resultsDF = self.sqlctx.sql('%s limit %s' %(query, num))
-                for result in resultsDF.toJSON().collect()
+                for result in resultsDF.toJSON().collect():
                     yield result
             except Py4JJavaError:
                 pass
