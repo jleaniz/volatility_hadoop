13ea759d08d972821a0ea16469c0fb85ed5a26d4
jleaniz <juan.leaniz@gmail.com>
2015-08-28 11:09:00 -0400

    refactoring some code in the UI

--- ui/app.py
+++ ui/app.py
@@ -6,26 +6,15 @@ from flask_bootstrap import (
     __version__ as FLASK_BOOTSTRAP_VERSION, Bootstrap
 )
 
-from forms import DateForm, SearchForm, UserDateForm, UserForm
 from nav import nav
 from engine import AnalyticsEngine
 
-import gzip
 import logging
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 main = Blueprint('main', __name__)
-
-@main.app_errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html', error=e.message)
-
-
-@main.app_errorhandler(500)
-def internal_server_error(e):
-    return render_template('500.html', error=e.message)
 
 
 @main.route("/api/vpn/byUser/<username>")
@@ -111,13 +100,6 @@ def bashKeyword(keyword):
     else:
         return 'Keyword or date unspecified.'
 
-@main.route("/bash/keyword", methods=('GET', 'POST'))
-def bash_keyword():
-    form = UserDateForm(csrf_enabled=False)
-    if form.validate_on_submit():
-        return redirect(
-            url_for('main.bashKeyword', keyword=form.name.data ))
-    return render_template("proxy.html", form=form)
 
 def buildJSON(table, fromdate, todate, query, num):
     jsonResult = analytics_engine.getSearchResults(table, fromdate, todate, query, num)
@@ -132,77 +114,6 @@ def buildJSON(table, fromdate, todate, query, num):
     return results
 
 
-@main.route('/download/<file>')
-def download(content):
-    f = gzip.open('/tmp/results.gz', 'wb')
-    try:
-        for line in content:
-            f.write(line)
-    finally:
-        f.close()
-
-    r = open('/tmp/results.gz', 'rb')
-    try:
-        buf = r.read()
-    finally:
-        r.close()
-
-    response = make_response(buf)
-    # This is the key: Set the right header for the response
-    # to be downloaded, instead of just printed on the browser
-    response.headers["Content-Disposition"] = "attachment; filename=results.gz"
-    return response
-
-
-@main.route("/vpn/user", methods=('GET', 'POST'))
-def vpn_user():
-    form = UserForm(csrf_enabled=False)
-    if form.validate_on_submit():
-        return redirect(url_for('main.vpnGoogleFormat', username=form.name.data))
-    return render_template("vpn.html", form=form)
-
-
-@main.route("/proxy/malware/user", methods=('GET', 'POST'))
-def proxy_user():
-    form = UserDateForm(csrf_enabled=False)
-    if form.validate_on_submit():
-        return redirect(
-            url_for('main.proxyGoogleFormat', username=form.name.data, fromdate=form.fromdate.data.strftime('%Y-%m-%d'),
-                    todate=form.todate.data.strftime('%Y-%m-%d')))
-    return render_template("proxy.html", form=form)
-
-
-@main.route("/proxy/top/transfers", methods=('GET', 'POST'))
-def proxyTopTransfers():
-    form = DateForm(csrf_enabled=False)
-    if form.validate_on_submit():
-        return redirect(url_for('main.getProxyTopTransfers', fromdate=form.fromdate.data.strftime('%Y-%m-%d'),
-                                todate=form.todate.data.strftime('%Y-%m-%d')))
-    return render_template("proxy.html", form=form)
-
-
-@main.route("/search", methods=('GET', 'POST'))
-def search_view():
-    Lookupform = SearchForm(csrf_enabled=False)
-
-    if Lookupform.validate_on_submit() and Lookupform.lookup.data:
-        return redirect(
-            url_for('main.search', table=Lookupform.table.data, fromdate=Lookupform.fromdate.data.strftime('%Y-%m-%d'),
-                    todate=Lookupform.todate.data.strftime('%Y-%m-%d'), query=Lookupform.query.data,
-                    num=Lookupform.num.data))
-    if Lookupform.validate_on_submit() and Lookupform.download.data:
-        data = buildJSON(Lookupform.table.data, Lookupform.fromdate.data.strftime('%Y-%m-%d'),
-                         Lookupform.todate.data.strftime('%Y-%m-%d'),
-                         Lookupform.query.data, Lookupform.num.data)
-        response = download(data)
-        return response
-
-    return render_template("search.html", form=Lookupform)
-
-
-@main.route('/')
-def index():
-    return render_template('index.html')
 
 
 def create_app(spark_context):
