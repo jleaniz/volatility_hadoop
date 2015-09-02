fa525f620769d45e0e08f943d21a697c753d9e84
jleaniz <juan.leaniz@gmail.com>
2015-09-02 10:42:48 -0400

    refactoring some code in the UI

--- ui/app.py
+++ ui/app.py
@@ -20,19 +20,20 @@ from pyspark import SparkContext, SparkConf
 from config import config as conf
 
 import logging
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 from flask_bootstrap import Bootstrap
-
 from flask import (
-    Flask, render_template, Blueprint
+    Flask, render_template, Blueprint, redirect, url_for, Response, make_response
 )
+from forms import DateForm, UserDateForm, SearchForm, CustomSearchForm
 from nav import nav
 from engine import AnalyticsEngine
-import bash, firewall, proxy, search, vpn
 
 main = Blueprint('main', __name__)
+
 
 def init_spark_context():
     # load spark context
@@ -42,8 +43,10 @@ def init_spark_context():
 
     return sc
 
+
 sc = init_spark_context()
 analytics_engine = AnalyticsEngine(sc)
+
 
 def run_server(app):
     # Enable WSGI access logging via Paste
@@ -67,6 +70,7 @@ def run_server(app):
     # Start the CherryPy WSGI web server
     cherrypy.engine.start()
     cherrypy.engine.block()
+
 
 @main.app_errorhandler(404)
 def page_not_found(e):
@@ -102,6 +106,198 @@ def buildJSONCustom(query):
     results.append('{}\n]}')
 
     return results
+
+
+@main.route('/proxy/LoginsByUser/google/<username>/<fromdate>/<todate>')
+def proxyGoogleFormat(username, fromdate, todate):
+    if username and fromdate and todate:
+        json = analytics_engine.getProxyUserMalwareHits(username, fromdate, todate)
+        logging.info(json)
+        return render_template('display_table.html', json=json)
+    else:
+        return 'Username or date unspecified.'
+
+
+@main.route('/proxy/topTransfers/google/<fromdate>/<todate>')
+def getProxyTopTransfers(fromdate, todate):
+    if fromdate and todate:
+        (jsonTable, jsonChart) = analytics_engine.getTopTransfersProxy(fromdate, todate)
+        # logging.info(jsonTable, jsonChart)
+        return render_template('proxyTopTransfers.html', jsonTable=jsonTable, jsonChart=jsonChart)
+    else:
+        return 'Date unspecified.'
+
+
+@main.route("/proxy/malware/user", methods=('GET', 'POST'))
+def proxy_user():
+    form = UserDateForm(csrf_enabled=False)
+    if form.validate_on_submit():
+        return redirect(
+            url_for('main.proxyGoogleFormat', username=form.name.data, fromdate=form.fromdate.data.strftime('%Y-%m-%d'),
+                    todate=form.todate.data.strftime('%Y-%m-%d')))
+    return render_template("proxy.html", form=form)
+
+
+@main.route("/proxy/top/transfers", methods=('GET', 'POST'))
+def proxyTopTransfers():
+    form = DateForm(csrf_enabled=False)
+    if form.validate_on_submit():
+        return redirect(url_for('main.getProxyTopTransfers', fromdate=form.fromdate.data.strftime('%Y-%m-%d'),
+                                todate=form.todate.data.strftime('%Y-%m-%d')))
+    return render_template("proxy.html", form=form)
+
+
+@main.route('/api/bash/keyword/<keyword>')
+def bashKeyword(keyword):
+    if keyword:
+        json = analytics_engine.bashKeywordSearch(keyword)
+        logger.info(json)
+        return render_template('display_table.html', json=json.decode('utf-8'))
+    else:
+        return 'Keyword or date unspecified.'
+
+
+@main.route("/bash/keyword", methods=('GET', 'POST'))
+def bash_keyword():
+    form = UserDateForm(csrf_enabled=False)
+    if form.validate_on_submit():
+        return redirect(
+            url_for('main.bashKeyword', keyword=form.name.data))
+    return render_template("proxy.html", form=form)
+
+
+@main.route("/api/vpn/byUser/<username>")
+def vpnJSON(username):
+    if username:
+        rdd = analytics_engine.getVPNLoginsByUserJSON(username)
+
+        def generate():
+            yield '{"%s": [\n' % (username)
+            for doc in rdd.collect():
+                yield doc + ',\n'
+            yield "{}\n]}"
+
+        return Response(generate(), mimetype='application/json')
+    else:
+        return 'Username unspecified.'
+
+
+@main.route("/api/vpn/identifyUser/<date>/<remoteip>")
+def identifyVPNAPI(date, remoteip):
+    if date and remoteip:
+        rdd = analytics_engine.identifyVPNUser(remoteip, date)
+
+        def generate():
+            yield '{"%s": [\n' % (remoteip)
+            for doc in rdd.collect():
+                yield doc + ',\n'
+            yield "{}\n]}"
+
+        return Response(generate(), mimetype='application/json')
+    else:
+        return 'Username unspecified.'
+
+
+@main.route('/vpn/LoginsByUser/google/<username>')
+def vpnGoogleFormat(username):
+    if username:
+        json = analytics_engine.getVPNLoginsByUserGoogle(username)
+        logger.info(json)
+        return render_template('vpnGC.html', json=json)
+    else:
+        return 'Username unspecified.'
+
+
+@main.route("/vpn/user", methods=('GET', 'POST'))
+def vpn_user():
+    form = UserForm(csrf_enabled=False)
+    if form.validate_on_submit():
+        return redirect(url_for('main.vpnGoogleFormat', username=form.name.data))
+    return render_template("vpn.html", form=form)
+
+
+@main.route('/download/<file>')
+def download(content):
+    f = gzip.open('/tmp/results.gz', 'wb')
+    try:
+        for line in content:
+            f.write(line)
+    finally:
+        f.close()
+
+    r = open('/tmp/results.gz', 'rb')
+    try:
+        buf = r.read()
+    finally:
+        r.close()
+
+    response = make_response(buf)
+    # This is the key: Set the right header for the response
+    # to be downloaded, instead of just printed on the browser
+    response.headers["Content-Disposition"] = "attachment; filename=results.gz"
+    return response
+
+
+@main.route('/search/<table>/<fromdate>/<todate>/<query>/<num>')
+def search(table, fromdate, todate, query, num):
+    jsonResult = analytics_engine.getSearchResults(table, fromdate, todate, query, num)
+
+    def generate():
+        yield '{"%s": [\n' % (table)
+        for doc in jsonResult:
+            yield doc + ',\n'
+        yield "{}\n]}"
+
+    return Response(generate(), mimetype='application/json')
+
+
+@main.route('/search/custom/<query>')
+def CustomSearch(query):
+    jsonResult = analytics_engine.getCustomSearchResults(query)
+
+    def generate():
+        yield '{"%s": [\n' % ('search')
+        for doc in jsonResult:
+            yield doc + ',\n'
+        yield "{}\n]}"
+
+    return Response(generate(), mimetype='application/json')
+
+
+@main.route("/search", methods=('GET', 'POST'))
+def search_view():
+    Lookupform = SearchForm(csrf_enabled=False)
+
+    if Lookupform.validate_on_submit() and Lookupform.lookup.data:
+        return redirect(
+            url_for('main.search', table=Lookupform.table.data, tables=Lookupform.tables,
+                    fromdate=Lookupform.fromdate.data.strftime('%Y-%m-%d'),
+                    todate=Lookupform.todate.data.strftime('%Y-%m-%d'), query=Lookupform.query.data,
+                    num=Lookupform.num.data))
+
+    if Lookupform.validate_on_submit() and Lookupform.download.data:
+        data = buildJSON(Lookupform.table.data, Lookupform.fromdate.data.strftime('%Y-%m-%d'),
+                         Lookupform.todate.data.strftime('%Y-%m-%d'),
+                         Lookupform.query.data, Lookupform.num.data)
+        response = download(data)
+        return response
+
+    return render_template("search.html", form=Lookupform)
+
+
+@main.route("/search/custom", methods=('GET', 'POST'))
+def custom_search_view():
+    Search = CustomSearchForm(csrf_enabled=False)
+
+    if Search.validate_on_submit() and Search.lookup.data:
+        return redirect(url_for('main.CustomSearch', query=Search.query.data))
+
+    if Search.validate_on_submit() and Search.download.data:
+        data = CustomSearch(Search.query.data)
+        response = download(data)
+        return response
+
+    return render_template("custom_search.html", form=Search)
 
 
 if __name__ == "__main__":
--- ui/engine.py
+++ ui/engine.py
@@ -24,8 +24,10 @@ import gviz_api
 import os
 
 import logging
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
 
 class AnalyticsEngine:
     '''
