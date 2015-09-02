6d4b246e19a6a4bbf511669278393e046a6a4fe9
jleaniz <juan.leaniz@gmail.com>
2015-09-02 15:39:47 -0400

    refactoring some code in the UI

--- ui/proxy.py
+++ ui/proxy.py
@@ -1,37 +1,20 @@
 from flask import (
-    render_template, redirect, url_for, Blueprint
+    render_template, Blueprint
 )
 from forms import UserDateForm, DateForm
 from engine import analytics_engine
 
 mod_proxy = Blueprint('proxy', __name__)
 
-@mod_proxy.route('/proxy/LoginsByUser/google/<username>/<fromdate>/<todate>')
-def proxyGoogleFormat(username, fromdate, todate):
-    if username and fromdate and todate:
-        json = analytics_engine.getProxyUserMalwareHits(username, fromdate, todate)
-        return render_template('display_table.html', json=json)
-    else:
-        return 'Username or date unspecified.'
-
-
-@mod_proxy.route('/proxy/topTransfers/google/<fromdate>/<todate>')
-def getProxyTopTransfers(fromdate, todate):
-    if fromdate and todate:
-        (jsonTable, jsonChart) = analytics_engine.getTopTransfersProxy(fromdate, todate)
-        # logging.info(jsonTable, jsonChart)
-        return render_template('proxyTopTransfers.html', jsonTable=jsonTable, jsonChart=jsonChart)
-    else:
-        return 'Date unspecified.'
-
 
 @mod_proxy.route("/proxy/malware/user", methods=('GET', 'POST'))
 def proxy_user():
     form = UserDateForm(csrf_enabled=False)
     if form.validate_on_submit():
-        return redirect(
-            url_for('main.proxyGoogleFormat', username=form.name.data, fromdate=form.fromdate.data.strftime('%Y-%m-%d'),
-                    todate=form.todate.data.strftime('%Y-%m-%d')))
+        json = analytics_engine.getProxyUserMalwareHits(form.name.data, form.fromdate.data.strftime('%Y-%m-%d'),
+                    form.todate.data.strftime('%Y-%m-%d'))
+        return render_template('display_table.html', json=json)
+
     return render_template("proxy.html", form=form)
 
 
@@ -39,6 +22,8 @@ def proxy_user():
 def proxyTopTransfers():
     form = DateForm(csrf_enabled=False)
     if form.validate_on_submit():
-        return redirect(url_for('main.getProxyTopTransfers', fromdate=form.fromdate.data.strftime('%Y-%m-%d'),
-                                todate=form.todate.data.strftime('%Y-%m-%d')))
+        (jsonTable, jsonChart) = analytics_engine.getTopTransfersProxy(form.fromdate.data.strftime('%Y-%m-%d'),
+                                                                       form.todate.data.strftime('%Y-%m-%d'))
+        return render_template('proxyTopTransfers.html', jsonTable=jsonTable, jsonChart=jsonChart)
+
     return render_template("proxy.html", form=form)
