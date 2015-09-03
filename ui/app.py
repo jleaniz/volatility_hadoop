3c01aad7db291ee4f0ce1227c9a7f8e0e963fad7
Juan Leaniz <juan.leaniz@gmail.com>
2015-09-03 01:23:31 -0400

    dashboard + firewall stats

--- ui/app.py
+++ ui/app.py
@@ -59,12 +59,12 @@ def run_server(app):
 
 @main.app_errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html', error=e.message)
+    return render_template('404.html', error=e.message), 404
 
 
 @main.app_errorhandler(500)
 def internal_server_error(e):
-    return render_template('500.html', error=e.message)
+    return render_template('500.html', error=e.message), 500
 
 
 @main.route('/')
