1ab1464b94df16b1051063e9743e76c8d6dcdfbb
Juan Leaniz <juan.leaniz@gmail.com>
2015-09-02 21:49:27 -0400

    dashboard + firewall stats

--- ui/dashboard.py
+++ ui/dashboard.py
@@ -7,7 +7,7 @@ mod_dashboard = Blueprint('dashboard', __name__)
 
 @mod_dashboard.route('/')
 def index():
-    redirect(url_for('mod_dashboard.Dashboard'))
+    redirect(url_for('dashboard.Dashboard'))
 
 @mod_dashboard.route("/dashboard", methods=('GET', 'POST'))
 def Dashboard():
--- ui/nav.py
+++ ui/nav.py
@@ -27,8 +27,8 @@ nav = Nav()
 # navbar has an usual amount of Link-Elements, more commonly you will have a
 # lot more View instances.
 nav.register_element('frontend_top', Navbar(
-    View('BDSA', '.index'),
-    View('Dashboard', '/dashboard'),
+    View('BDSA', 'main.index'),
+    View('Dashboard', 'dashboard.Dashboard'),
     Subgroup(
         'Analytics',
         Text('VPN'),
