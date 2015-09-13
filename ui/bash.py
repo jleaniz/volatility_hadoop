bec6104a730700ed61301d1e6305a2733003ccfe
Juan Leaniz <juan.leaniz@gmail.com>
2015-09-12 22:51:35 -0400

    added kmeans clustering results 2 html

--- ui/bash.py
+++ ui/bash.py
@@ -33,8 +33,8 @@ def index():
 def bash_kmeans():
     form = KeywordForm(csrf_enabled=False)
     if form.validate_on_submit():
-        (command, vector, cluster, syms, uncommon) = analytics_engine.getCmdPrediction(form.keyword.data)
-        return render_template('bash_kmeans.html', command=command, vector=vector, syms=syms, uncommon=uncommon)
+        (command, vector, cluster, clustersDict, syms, uncommon) = analytics_engine.getCmdPrediction(form.keyword.data)
+        return render_template('bash_kmeans.html', command=command, vector=vector, clustersDict=clustersDict, syms=syms, uncommon=uncommon)
 
     return render_template("bash.html", form=form)
 
