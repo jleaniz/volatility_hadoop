0ad42dba72d4e6476ab76a1673603fa17d2dd71c
jleaniz <juan.leaniz@gmail.com>
2015-09-22 11:55:44 -0400

    timeline file type count

--- ui/engine.py
+++ ui/engine.py
@@ -324,7 +324,8 @@ class AnalyticsEngine(object):
 
         for day in days:
             try:
-                if table is not 'proxysg':
+                print table
+                if table != 'proxysg':
                     filteredDF = tempDF.filter(
                         'year=%s and month=%s and day=%s' % (day.year, str(day).split('-')[1], str(day).split('-')[2]))
                     self.sqlctx.registerDataFrameAsTable(filteredDF, table)
