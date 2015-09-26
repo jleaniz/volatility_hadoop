88b95d93b276283105cab6e51e341d571c50a1ae
Juan Leaniz <juan.leaniz@gmail.com>
2015-09-26 18:22:51 -0400

    getc2feeds fix

--- ingest/feeds.py
+++ ingest/feeds.py
@@ -42,6 +42,7 @@ def updateAlienvaultOtx(sContext):
 def getC2Feeds():
     results = []
     urls = ['http://osint.bambenekconsulting.com/feeds/c2-masterlist.txt',
+            'http://rules.emergingthreats.net/fwrules/emerging-IPF-CC.rules',
             'https://lists.malwarepatrol.net/cgi/getfile?'
             'http://mirror1.malwaredomains.com/files/domains.txt',
             'http://www.montanamenagerie.org/hostsfile/hosts.txt'
