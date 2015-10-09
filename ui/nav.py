80c231e33a66d0a3b20c6326f0f52d6202b08431
jleaniz <juan.leaniz@gmail.com>
2015-10-09 13:04:04 -0400

    firewall top talkers

--- ui/nav.py
+++ ui/nav.py
@@ -29,7 +29,7 @@ nav = Nav()
 # navbar has an usual amount of Link-Elements, more commonly you will have a
 # lot more View instances.
 nav.register_element('frontend_top', Navbar(
-    View('Graphene (0.1a)', 'main.index'),
+    View('BDSA (0.1a)', 'main.index'),
     View('Dashboard', 'dashboard.Dashboard'),
     Subgroup(
         'Analytics',
