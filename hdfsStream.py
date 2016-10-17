a734d3cb340387e99a5f5cc3ccce56471cccaf2f
jleaniz <juan.leaniz@gmail.com>
2016-10-17 16:28:50 -0400

    config: memory allocation

--- hdfsStream.py
+++ hdfsStream.py
@@ -64,8 +64,8 @@ def save_proxy(rdd):
 def process_fw(time, rdd):
     if not rdd.isEmpty():
         output_rdd = rdd.filter(lambda x: '-fw' in x)\
-            .map(parse) \
-            .filter(lambda x: isinstance(x, Row))
+            #.map(parse) \
+            #.filter(lambda x: isinstance(x, Row))
         print output_rdd.first()
         return output_rdd
 
