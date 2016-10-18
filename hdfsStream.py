f8366eee346fe2494196b56cbd346d63c5a5eb08
jleaniz <juan.leaniz@gmail.com>
2016-10-17 21:24:22 -0400

    parser: add new parser function

--- hdfsStream.py
+++ hdfsStream.py
@@ -49,7 +49,7 @@ class batchInfoCollector(StreamingListener):
             .outputOperationInfos()[0]
             .endTime() / 1000)
         logger.warning('batchDate: ', batchDate)
-        if last_updated - batchDate > datetime.timedelta(minutes=1):
+        if batchDate - last_updated > datetime.timedelta(minutes=1):
             logger.warning('Date has changed, restarting StreamingContext...')
             StreamingContext.getActive().stop(stopSparkContext=False)
 
