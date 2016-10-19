9089ee9b62de82c40b7892e1067212bde47fdb41
jleaniz <juan.leaniz@gmail.com>
2016-10-19 10:28:39 -0400

    engine.py : spark context update

--- hdfsStream.py
+++ hdfsStream.py
@@ -48,7 +48,7 @@ class batchInfoCollector(StreamingListener):
             .outputOperationInfos()[0]
             .endTime() / 1000)
         logger.warning('batchDate: ' + str(batchDate))
-        if batchDate - last_updated > datetime.timedelta(days=1):
+        if batchDate - last_updated > datetime.timedelta(minutes=3):
             logger.warning('Date has changed, Stopping StreamingContext.')
             del self.batchInfosCompleted[:]
             StreamingContext.getActive().stop(stopSparkContext=False, stopGraceFully=True)
