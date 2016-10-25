0a9a82fb3915ce8dcd93e4eea325a1716aae205a
jleaniz <juan.leaniz@gmail.com>
2016-10-25 14:27:02 -0400

    exception handling

--- ui/engine.py
+++ ui/engine.py
@@ -954,6 +954,7 @@ class AnalyticsEngine(object):
             self.sccmDF = self.session.read.parquet('/user/jleaniz/sccm/df_sys_dsA1')
         except Exception as e:
             logger.warning(e)
+            return
 
         self.sc.setLocalProperty("spark.scheduler.pool", "dashboard")
 
