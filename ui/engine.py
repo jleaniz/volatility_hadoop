793237022aca5b6a736314aab9affc16bc199e90
jleaniz <juan.leaniz@gmail.com>
2016-10-31 13:38:54 -0400

    fix: bash parser

--- ui/engine.py
+++ ui/engine.py
@@ -1101,7 +1101,7 @@ class AnalyticsEngine(object):
 
         adlocation.cache()
 
-        vpn = self.session.read.parquet('/user/jleaniz/ciscovpn')
+        vpn = self.session.read.parquet('/data/srm/dbs/dw_srm.db/ciscovpn').rdd
         vpn.cache()
 
         def func(x):
