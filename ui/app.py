af5a21b9cd435ab0694964b6ca2bfb9422851c86
jleaniz <juan.leaniz@gmail.com>
2016-10-31 19:46:08 -0400

    AAD auth

--- ui/app.py
+++ ui/app.py
@@ -14,11 +14,10 @@
 # You should have received a copy of the GNU General Public License
 # along with BDSA.  If not, see <http://www.gnu.org/licenses/>.
 #
-import logging
-import cherrypy
 from paste.translogger import TransLogger
 from flask_bootstrap import Bootstrap
-from flask import Flask, render_template, Blueprint, send_from_directory, flash, redirect, url_for
+from flask import Flask, render_template, Blueprint, send_from_directory, flash, redirect, url_for, Response, request, session
+from functools import wraps
 from nav import nav
 from search import mod_search
 from firewall import mod_firewall
@@ -29,11 +28,74 @@ from dashboard import mod_dashboard
 from engine import analytics_engine
 from forensics import mod_for
 from patch_mgmt import mod_pm_dashboard
+from cryptography.x509 import load_pem_x509_certificate
+from cryptography.hazmat.backends import default_backend
+import adal
+import Cookie
+import json
+import sys
+import os
+import random
+import string
+import base64
+import jwt
+import logging
+import cherrypy
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-
 main = Blueprint('main', __name__)
+
+parameters_file = (sys.argv[1] if len(sys.argv) == 2 else
+                   os.getcwd()+'/ADAL_PARAMETERS')
+
+if parameters_file:
+    with open(parameters_file, 'r') as f:
+        parameters = f.read()
+    adal_parameters = json.loads(parameters)
+else:
+    raise ValueError('Please provide parameter file with account information.')
+
+TEMPLATE_AUTHZ_URL = ('https://login.windows.net/{}/oauth2/authorize?'+
+                      'response_type=id_token+code&response_mode=form_post&client_id={}&redirect_uri={}&'+
+                      'state={}&nonce={}&resource={}')
+
+
+
+#access token is required in session
+def access_token_required(func):
+    @wraps(func)
+    def __decorator():
+        if not session.get('id_token'):
+            return redirect(url_for('index'))
+        return func()
+
+    return __decorator
+
+
+def validate_id_token(id_token):
+    try:
+        f = open(adal_parameters['idp_cert'], 'r')
+        cert_str = f.read()
+        f.close()
+    except IOError as e:
+        print('Unable to open PEM certificate')
+        return False
+
+    cert_obj = load_pem_x509_certificate(cert_str, default_backend())
+    public_key = cert_obj.public_key()
+
+    print public_key
+
+    try:
+        token = jwt.decode(id_token,
+                           public_key,
+                           algorithms=['RS256'],
+                           audience=adal_parameters['clientId'])
+    except Exception as e:
+        return False
+
+    return True
 
 
 def secureheaders():
@@ -43,6 +105,7 @@ def secureheaders():
     headers['Content-Security-Policy'] = "default-src='self'"
     if (cherrypy.server.ssl_certificate != None and cherrypy.server.ssl_private_key != None):
         headers['Strict-Transport-Security'] = 'max-age=31536000' # one year
+
 
 def run_server(app):
     # Enable WSGI access logging via Paste
@@ -87,8 +150,47 @@ def serve_file(filename):
 
 
 @main.route('/')
+@access_token_required
 def index():
     return render_template('index.html')
+
+
+@main.route('/login')
+def login():
+        auth_state = (''.join(random.SystemRandom()
+                .choice(string.ascii_uppercase + string.digits)
+                for _ in range(48)))
+
+        nonce = auth_state
+        cookie = Cookie.SimpleCookie()
+        cookie['auth_state'] = auth_state
+        authorization_url = TEMPLATE_AUTHZ_URL.format(
+                adal_parameters['tenant'],
+                adal_parameters['clientId'],
+                adal_parameters['redirect_uri'],
+                auth_state,
+                nonce,
+                adal_parameters['resource'])
+
+        redirect_to_AAD = redirect(authorization_url)
+        response = app.make_response(redirect_to_AAD)
+        response.set_cookie('auth_state', auth_state)
+        return response
+
+
+@main.route('/login/callback', methods=['GET','POST'])
+def login_callback():
+        # Verify AAD id_token
+        id_token = request.form['id_token']
+        code = request.form['code']
+
+        if id_token:
+                if validate_id_token(id_token):
+                        session['access_token'] = id_token
+                else:
+                        return Response(json.dumps({'auth': 'error: invalid token'}), mimetype='application/json')
+        else:
+                return Response(json.dumps({'auth': 'error: no token found'}), mimetype='application/json')
 
 
 @main.route('/spark/clearcache')
