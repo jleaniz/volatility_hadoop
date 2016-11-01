b8469293940ddd443be5bd529c3720c2b4044f30
jleaniz <juan.leaniz@gmail.com>
2016-10-31 20:26:54 -0400

    AAD auth

--- ui/app.py
+++ ui/app.py
@@ -188,7 +188,7 @@ def login_callback():
 
         if id_token:
                 if validate_id_token(id_token):
-                        session['access_token'] = id_token
+                        session['id_token'] = id_token
                         return render_template('index.html')
                 else:
                         return Response(json.dumps({'auth': 'error: invalid token'}), mimetype='application/json')
